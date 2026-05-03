df5dfff9a900652dc1ee2be6815c829da2b02288
IFerowsky <norbert.plich@o2.pl>
2026-05-03 14:07:22 +0200

    refactor: simplify Rotated Digits solution by removing unnecessary vector and string manipulations

--- .dailyChallenes/20260502_RotatedDigits/main.cpp
+++ .dailyChallenes/20260502_RotatedDigits/main.cpp
@@ -1,63 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
 
 int main(){
     int T=0;
     std::cin >> T;
 
     while(T--){
-        std::vector<std::string> v;
-        int n=0;
-        std::cin>>n;
+        int n=0,counter=0;
+        std::cin >> n;
 
-        for(int i=0;i<n;i++){
-           v.push_back(std::to_string(i+1)); 
-        }
-
-        int counter = 0;
-        for(auto number : v){
-            std::string temp = "";
-            for(int i=0;i<(int)number.size();i++){
-                char element = number[i];
-                if(element == '0' || element == '1' || element == '8'){
-                    temp.append(1,element);
-                }else if(element == '2'){
-                    temp.append(1,'5');
-                }else if(element == '5'){
-                    temp.append(1,'2');
-                }else if(element == '6'){
-                    temp.append(1,'9');
-                }else if(element == '9'){
-                    temp.append(1,'6');
-                }else{
-                    temp="";
+        for(int i=1;i<=n;i++){
+            int x = i;
+            bool isGood=false;
+            while(x>0){
+                int digit = x%10;
+                
+                if(digit==3||digit==4||digit==7){
+                    isGood = false;
                     break;
+                }else if(digit==2||digit==5||digit==6||digit==9){
+                    isGood = true;
                 }
-            }
 
-            //std::cout <<number <<" -> "<< temp << "\n";
-            //std::cout <<"======================\n";
-
-            int p1=0,p2=0;
-            while(p1<(int)number.size()&&p2<(int)temp.size()){
-                //std::cout<<number[p1]<<" "<<temp[p2]<<"\n";
-                if(number[p1]==temp[p2]){
-                    p1++;
-                    p2++;
-                }else{
-                    counter++;
-                    //std::cout<<"rozne!\n";
-                    break;
-                }
-                //std::cout<<"takie same!\n";
+                x /= 10;                    
             }
-            //std::cout <<"======================\n";
+            if(isGood) counter++;
         }
-        std::cout<<counter<<"\n";
+        std::cout << counter << "\n";
     }
     return 0;
 }
